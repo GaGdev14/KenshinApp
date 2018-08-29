00bdab1288858093e18f499c5a9b9be4279180ec
InaShow <gagdev14@gmail.comls>
2018-08-29 19:42:57 +0900

    Initial Commit

---
+++ KenshinApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KenshinApp
+//
+//  Created by 1490402 on 2018/08/29.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
