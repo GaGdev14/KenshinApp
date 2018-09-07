89ec44cd7612f59e89f78128c046d64f45ff95f6
MaiInagaki <maiinagaki@maiinagakinomacbook-puro.local>
2018-09-07 18:23:24 +0900

    指示数入力値確認用画面追加（ResultReport.storyboard）。また、それ用のVCResultReport（ViewController）を追加。

---
+++ KenshinApp/Button_Custom.swift
@@ -0,0 +1,34 @@
+//
+//  Button_Custom.swift
+//  KenshinApp
+//
+//  Created by MaiInagaki on 2018/09/07.
+//  Copyright © 2018年 GaGdev14. All rights reserved.
+//
+
+
+import UIKit
+@IBDesignable
+class Button_Custom: UIButton {
+    
+    @IBInspectable var textColor: UIColor?
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    /* @IBInspectable var borderColor: UIColor = UIColor.clearColor() {
+     didSet {
+     layer.borderColor = borderColor.CGColor
+     }
+     
+     } */
+}
